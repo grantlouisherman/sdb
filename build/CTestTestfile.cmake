0e0c9381cbdc31491b995d787bae793e47916698
Grant Herman <grantlouisherman041@gmail.com>
2026-03-20 02:39:01 +0000

    init

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /app/sdb
+# Build directory: /app/sdb/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("tools")
+subdirs("test")
